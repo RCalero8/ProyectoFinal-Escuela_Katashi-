bf458ff70eb9300867c8f67166aa28dff5ce40fe
RCalero8 <calerorocio13@gmail.com>
2026-03-15 09:33:16 +0100

    Solucion error backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors    = require("cors");
-require("dotenv").config();
+const conexion = require("./config/db");  
 
 const rutasNoticias = require("./routes/noticias");
 const rutasCurso = require("./routes/cursos");
