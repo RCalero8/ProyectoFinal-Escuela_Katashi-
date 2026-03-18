d4a5a0a3a83760f293f2e89471d8c2aeca7ba8d8
RCalero8 <calerorocio13@gmail.com>
2026-03-18 17:06:35 +0100

    Hero conocenos

---
+++ fronted/src/components/Invitado/Conocenos/Hero.tsx
@@ -0,0 +1,17 @@
+import "../../../style/Invitado/Conocenos/Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-overlay" />
+      <div className="hero-content">
+        <h1 className="hero-titulo">Bienvenido a la Escuela Karate Katashi</h1>
+        <p className="hero-descripcion">
+          Descubre nuestro espíritu, historia y valores
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
