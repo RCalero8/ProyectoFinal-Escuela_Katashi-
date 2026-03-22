67a5f401a4865a2e753d9b6532c20e0c781d6972
RCalero8 <calerorocio13@gmail.com>
2026-03-22 16:44:40 +0100

    tipos(senseis) front

---
+++ fronted/src/tipos/senseis.ts
@@ -0,0 +1,9 @@
+export interface Sensei {
+    id_info: number;
+    id_usuario: number;
+    dan: string;
+    rol_web: string;
+    biografia: string;
+    nombre: string;
+    apellido: string;
+}
