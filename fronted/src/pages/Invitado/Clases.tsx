1301f913ea5fe9353138b843b9ea5fc7095f27f5
RCalero8 <calerorocio13@gmail.com>
2026-04-09 18:18:37 +0200

    Cursos de pagina clase

--- fronted/src/pages/Invitado/Clases.tsx
+++ fronted/src/pages/Invitado/Clases.tsx
@@ -1,11 +1,12 @@
 import Hero from "../../components/Invitado/Clases/Hero";
+import Cursos from "../../components/Invitado/Clases/Cursos";
 
 
 const Clases = () => {
     return (
         <>
         <Hero />
-    
+        <Cursos />
        </>
     )
 }
