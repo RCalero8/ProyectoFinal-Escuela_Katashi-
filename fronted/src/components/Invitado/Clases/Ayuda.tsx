0b227ec97092a7aefc40cda1fc0259d84bb6d86b
RCalero8 <calerorocio13@gmail.com>
2026-04-11 09:29:04 +0200

    Ayuda pagina Curso v3

--- fronted/src/components/Invitado/Clases/Ayuda.tsx
+++ fronted/src/components/Invitado/Clases/Ayuda.tsx
@@ -4,19 +4,13 @@ import '../../../style/Invitado/Clases/Ayuda.css';
 const Ayuda: React.FC = () => {
 
     return (
-    <section className="help-banner">
-      <h2 className="help-banner__title">Necesitas Ayuda</h2>
-      <p className="help-banner__text">
-        Si necesitas ayuda para elegir tu clase, contáctanos.
-      </p>
-      <a 
-        href="/contacto" 
-        className="help-banner__button" 
-        aria-label="Contactar con la escuela"
-      >
-        Contactar con la escuela
-      </a>
-    </section>
+      <div className="help-banner-wrapper">
+        <section className="help-banner">
+          <h2 className="help-banner_titulo">Necesitas Ayuda</h2>
+          <p className="help-banner_texto">Si necesitas ayuda para elegir tu clase, contáctanos</p>
+          <a href='/contacto' className='help-banner_boton' aria-label='Contactar con la escuela'>Contactar con la escuela</a>
+        </section>
+      </div>
   );
 };
 
