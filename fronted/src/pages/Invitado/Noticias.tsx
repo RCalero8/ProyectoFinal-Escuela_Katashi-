09d1e787848a465157953f940edcbf9903b3a313
RCalero8 <calerorocio13@gmail.com>
2026-04-14 16:47:40 +0200

    Filtro de Noticias v2

--- fronted/src/pages/Invitado/Noticias.tsx
+++ fronted/src/pages/Invitado/Noticias.tsx
@@ -1,8 +1,12 @@
 import Hero from "../../components/Invitado/Noticias/Hero";
+import Filtros from "../../components/Invitado/Noticias/Filtros";
 
 const Noticias = () => {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <Filtros />
+    </>
   );
 };
 
