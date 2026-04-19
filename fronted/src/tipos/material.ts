f13a88de8c86e7d64a0d28a66f22727e35e2ed5a
RCalero8 <calerorocio13@gmail.com>
2026-04-19 13:20:53 +0200

    Material.ts

---
+++ fronted/src/tipos/material.ts
@@ -0,0 +1,11 @@
+export interface Material {
+    id_material: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    stock: number;
+    Categoria: string;
+    Imagen: string;
+    Talla: string;
+    Color: string | null;
+}
