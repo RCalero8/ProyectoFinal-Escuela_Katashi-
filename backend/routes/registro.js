37daf2142623a5b41af50f8036c9104663104e24
RCalero8 <calerorocio13@gmail.com>
2026-04-26 17:31:17 +0200

    Back de Registro v2

--- backend/routes/registro.js
+++ backend/routes/registro.js
@@ -1,6 +1,6 @@
-const express = requiere ('express');
+const express = require ('express');
 const router = express.Router();
-const pool = requiere('../config/db');
+const pool = require('../config/db');
 
 //Post /api/registro
 router.post('/', async (req, res) => {
