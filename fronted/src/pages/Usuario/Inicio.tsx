0fa9dae88a05cca2eb87b0c29d55dd8d976ccfab
RCalero8 <calerorocio13@gmail.com>
2026-05-06 18:46:51 +0200

    Inicio usuario LatestNew v1

--- fronted/src/pages/Usuario/Inicio.tsx
+++ fronted/src/pages/Usuario/Inicio.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/usuario/Header";
 import Hero from "../../components/Usuario/Inicio/Hero";
 import Dashboard from "../../components/Usuario/Inicio/UserDashboard";
+import News from "../../components/Usuario/Inicio/LatestNews";
 
 const Inicio = () => {
   return (
@@ -8,6 +9,7 @@ const Inicio = () => {
       <Header />
       <Hero />
       <Dashboard />
+      <News />
     </>
   );
 };
