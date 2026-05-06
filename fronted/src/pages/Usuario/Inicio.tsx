42416fbc05e9dc2cb9dee1ab706397b10ab0aa26
RCalero8 <calerorocio13@gmail.com>
2026-05-06 14:09:57 +0200

    Inicio-header b1

---
+++ fronted/src/pages/Usuario/Inicio.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/usuario/Header";
+
+const Inicio = () => {
+  return (
+    <>
+      <Header />
+      {/* Aquí irán el resto de secciones */}
+    </>
+  );
+};
+
+export default Inicio;
