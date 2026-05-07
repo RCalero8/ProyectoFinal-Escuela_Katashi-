262dd409154bedb8be67cd43bcad69598d79b5ca
RCalero8 <calerorocio13@gmail.com>
2026-05-07 17:25:49 +0200

    Añadimos Horario a la pagina de inicio

--- fronted/src/pages/Usuario/Inicio.tsx
+++ fronted/src/pages/Usuario/Inicio.tsx
@@ -2,7 +2,7 @@ import Header from "../../components/Header/usuario/Header";
 import Hero from "../../components/Usuario/Inicio/Hero";
 import Dashboard from "../../components/Usuario/Inicio/UserDashboard";
 import News from "../../components/Usuario/Inicio/LatestNews";
-
+import Horario from "../../components/Usuario/Inicio/Iniciousuariosecciones";
 const Inicio = () => {
   return (
     <>
@@ -10,6 +10,7 @@ const Inicio = () => {
       <Hero />
       <Dashboard />
       <News />
+      <Horario />
     </>
   );
 };
