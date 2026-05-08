604baff6447cc5ebe68ae5d17d474b28cb3691b1
RCalero8 <calerorocio13@gmail.com>
2026-05-08 11:43:37 +0200

    Remove unused import of useNavigate

--- fronted/src/components/Usuario/Mis_Clases/proximasclases.tsx
+++ fronted/src/components/Usuario/Mis_Clases/proximasclases.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "../../../style/Usuario/MIs_Clases/proximasclases.css";
 import ModalHorario from "./Modalhorario";
  
