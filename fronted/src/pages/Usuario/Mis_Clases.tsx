40ebf1f792f239e20891ab8e5a121482763e481e
RCalero8 <calerorocio13@gmail.com>
2026-05-08 16:21:06 +0200

    Añadimos a la pagina Mis clases su cuarta parte

--- fronted/src/pages/Usuario/Mis_Clases.tsx
+++ fronted/src/pages/Usuario/Mis_Clases.tsx
@@ -2,6 +2,7 @@ import Header from "../../components/Header/usuario/Header";
 import MisClases from "../../components/Usuario/Mis_Clases/mis_clases";
 import ProxClase from "../../components/Usuario/Mis_Clases/proximasclases";
 import Historia from "../../components/Usuario/Mis_Clases/Historialasistencia";
+import Gestionar from "../../components/Usuario/Mis_Clases/Gestionar_inscripciones";
 import {Footer} from "../../components/Footer/Usuario/Footer";
 
 const Mis_clases = () => {
@@ -11,6 +12,7 @@ const Mis_clases = () => {
       <MisClases />
       <ProxClase/>
       <Historia />
+      <Gestionar />
       <Footer />
     </>
   );
