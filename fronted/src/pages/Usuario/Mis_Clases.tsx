f7a8fc3bc2941db0c3cc047c5e11ea0d3e9dab5e
RCalero8 <calerorocio13@gmail.com>
2026-05-08 12:46:04 +0200

    Add Modal component to Mis_clases page

--- fronted/src/pages/Usuario/Mis_Clases.tsx
+++ fronted/src/pages/Usuario/Mis_Clases.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/usuario/Header";
 import MisClases from "../../components/Usuario/Mis_Clases/mis_clases";
 import ProxClase from "../../components/Usuario/Mis_Clases/proximasclases";
+import Modal from "../../components/Usuario/Mis_Clases/Modalhorario";
 import {Footer} from "../../components/Footer/Usuario/Footer";
 
 const Mis_clases = () => {
@@ -9,6 +10,7 @@ const Mis_clases = () => {
       <Header />
       <MisClases />
       <ProxClase/>
+      <Modal />
       <Footer />
     </>
   );
