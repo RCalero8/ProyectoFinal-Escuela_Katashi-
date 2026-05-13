873139e7f4be0a42730c9f5b1e8020c780b96c2e
RCalero8 <calerorocio13@gmail.com>
2026-05-13 08:40:23 +0200

    Añadimos Resumen a Federacion.

--- fronted/src/pages/Usuario/Federacion.tsx
+++ fronted/src/pages/Usuario/Federacion.tsx
@@ -1,10 +1,11 @@
 import Hero from "../../components/Usuario/Federacion/Hero";
-
+import Resumen from "../../components/Usuario/Federacion/Resumen";
 
 const Federacion = () => {
   return (
     <>
-     <Hero />
+      <Hero />
+      <Resumen />
     </>
   );
 };
