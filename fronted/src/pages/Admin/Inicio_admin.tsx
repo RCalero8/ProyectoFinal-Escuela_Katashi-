318e8b8e0fb0926a5d33a1ccc523f15bccb43e7f
RCalero8 <calerorocio13@gmail.com>
2026-05-13 13:38:43 +0200

    Create Inicio_admin component with Header and Footer

---
+++ fronted/src/pages/Admin/Inicio_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Inicio from "../../components/Admin/Inicio_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Inicio_admin = () => {
+  return (
+    <>
+      <Header />
+      <Inicio />
+      <Footer />
+    </>
+  );
+};
+
+export default Inicio_admin;
