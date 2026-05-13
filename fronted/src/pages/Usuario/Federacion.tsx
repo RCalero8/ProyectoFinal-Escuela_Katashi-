c8edeba3cc3dd829a91017da5f13272fb4d6f093
RCalero8 <calerorocio13@gmail.com>
2026-05-13 08:55:35 +0200

    Add Avisos component to Federacion page

--- fronted/src/pages/Usuario/Federacion.tsx
+++ fronted/src/pages/Usuario/Federacion.tsx
@@ -1,12 +1,14 @@
 import Hero from "../../components/Usuario/Federacion/Hero";
 import Resumen from "../../components/Usuario/Federacion/Resumen";
 import Documentacion from "../../components/Usuario/Federacion/Documentacion";
+import Documentacion from "../../components/Usuario/Federacion/Avisos";
 const Federacion = () => {
   return (
     <>
       <Hero />
       <Resumen />
       <Documentacion />
+      <Avisos/>
     </>
   );
 };
