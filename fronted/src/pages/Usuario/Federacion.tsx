c163f796512e47c689ed4914ac6562841e55bcb6
RCalero8 <calerorocio13@gmail.com>
2026-05-13 08:49:41 +0200

    Add Documentacion component to Federacion page

--- fronted/src/pages/Usuario/Federacion.tsx
+++ fronted/src/pages/Usuario/Federacion.tsx
@@ -1,11 +1,12 @@
 import Hero from "../../components/Usuario/Federacion/Hero";
 import Resumen from "../../components/Usuario/Federacion/Resumen";
-
+import Documentacion from "../../components/Usuario/Federacion/Documentacion";
 const Federacion = () => {
   return (
     <>
       <Hero />
       <Resumen />
+      <Documentacion />
     </>
   );
 };
