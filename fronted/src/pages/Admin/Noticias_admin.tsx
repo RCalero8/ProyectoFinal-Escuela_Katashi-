262552bb45111a5953c4245123c032b0ecfc3075
RCalero8 <calerorocio13@gmail.com>
2026-05-14 14:35:55 +0200

    Add Noticias_admin component with header and footer

---
+++ fronted/src/pages/Admin/Noticias_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Noticia from "../../components/Admin/Noticias_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Noticias_admin = () => {
+  return (
+    <>
+      <Header />
+      <Noticia />
+      <Footer />
+    </>
+  );
+};
+
+export default Noticias_admin;
