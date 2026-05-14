b925bc60c281f6ff3e27460531d2498d8fd9fcd0
RCalero8 <calerorocio13@gmail.com>
2026-05-14 14:34:25 +0200

    Create Tienda_admin component with Header and Footer

---
+++ fronted/src/pages/Admin/Tienda_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Tienda from "../../components/Admin/Tienda_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Tienda_admin = () => {
+  return (
+    <>
+      <Header />
+      <Tienda />
+      <Footer />
+    </>
+  );
+};
+
+export default Tienda_admin;
