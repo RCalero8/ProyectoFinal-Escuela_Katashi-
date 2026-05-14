4390deef1c02eaf708e708c5dabc05dcec4e8d41
RCalero8 <calerorocio13@gmail.com>
2026-05-14 14:14:53 +0200

    Create Horarios_admin component with Header and Footer

---
+++ fronted/src/pages/Admin/Horarios_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Horario from "../../components/Admin/Horarios_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Alumnos_admin = () => {
+  return (
+    <>
+      <Header />
+      <Horario />
+      <Footer />
+    </>
+  );
+};
+
+export default Alumnos_admin;
