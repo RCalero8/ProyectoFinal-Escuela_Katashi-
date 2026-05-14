b8268b9e6a0583634c258c57b8ea29a469923e4e
RCalero8 <calerorocio13@gmail.com>
2026-05-14 14:32:45 +0200

    Create Asistencias_admin component with layout

---
+++ fronted/src/pages/Admin/Asistencias_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Asistencia from "../../components/Admin/Asistencias_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Asistencias_admin = () => {
+  return (
+    <>
+      <Header />
+      <Asistencia />
+      <Footer />
+    </>
+  );
+};
+
+export default Asistencias_admin;
