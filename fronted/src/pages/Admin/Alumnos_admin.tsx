82ac590f1da44ff9bd8c3cfd35901bac7aef15c7
RCalero8 <calerorocio13@gmail.com>
2026-05-14 13:42:34 +0200

    Create Alumnos_admin component with Header and Footer

---
+++ fronted/src/pages/Admin/Alumnos_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Alumno from "../../components/Admin/Alumnos_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Alumnos_admin = () => {
+  return (
+    <>
+      <Header />
+      <Alumno />
+      <Footer />
+    </>
+  );
+};
+
+export default Alumnos_admin;
