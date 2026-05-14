52727ebbd59ef3835094172e12eec2ff7563afdb
RCalero8 <calerorocio13@gmail.com>
2026-05-14 14:40:23 +0200

    Fix import path for Asistencia component

--- fronted/src/pages/Admin/Asistencias_admin.tsx
+++ fronted/src/pages/Admin/Asistencias_admin.tsx
@@ -1,5 +1,5 @@
 import Header from "../../components/Header/Admin/Header";
-import Asistencia from "../../components/Admin/Asistencias_admin";
+import Asistencia from "../../components/Admin/Asistencia_admin";
 import Footer from "../../components/Footer/Admin/Footer";
 
 const Asistencias_admin = () => {
