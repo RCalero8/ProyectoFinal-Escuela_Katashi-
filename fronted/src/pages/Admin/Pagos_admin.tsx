829a736a4fa8e49efb64dd411b67722bacc21af9
RCalero8 <calerorocio13@gmail.com>
2026-05-14 14:07:40 +0200

    Create Pagos_admin component with header and footer

---
+++ fronted/src/pages/Admin/Pagos_admin.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Admin/Header";
+import Pagos from "../../components/Admin/Pagos_admin";
+import Footer from "../../components/Footer/Admin/Footer";
+
+const Alumnos_admin = () => {
+  return (
+    <>
+      <Header />
+      <Pagos />
+      <Footer />
+    </>
+  );
+};
+
+export default Alumnos_admin;
