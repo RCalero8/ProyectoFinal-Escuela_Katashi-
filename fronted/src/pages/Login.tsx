f67db955c1a924a610997f1a77dc307af336f9f1
RCalero8 <calerorocio13@gmail.com>
2026-05-15 13:33:09 +0200

    Add Header and Footer components to Login page

--- fronted/src/pages/Login.tsx
+++ fronted/src/pages/Login.tsx
@@ -1,10 +1,14 @@
 import LoginHero from "../components/Login/LoginHero";
 import LoginForm from "../components/Login/Loginform";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 const Login = () => {
     return (
         <>
+            <Header/>
             <LoginHero/>
             <LoginForm/>
+            <Footer/>
        </>
     )
 }
