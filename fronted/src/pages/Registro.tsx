1471629b9e539ff02347878d523135253275746e
RCalero8 <calerorocio13@gmail.com>
2026-05-15 13:35:45 +0200

    Add Header and Footer components to Registro page

--- fronted/src/pages/Registro.tsx
+++ fronted/src/pages/Registro.tsx
@@ -1,8 +1,12 @@
 import RegistroForm from "../components/Registro/Registroform";
+import {Header} from "../components/Header/Header";
+import {Footer} from "../components/Footer/Footer";
 const Registro = () => {
     return (
         <>
+            <Header/>
           <RegistroForm/>
+            <Footer/>
        </>
     )
 }
