67c9784c76bd713e9e99b160ef7359aed3c456f9
RCalero8 <calerorocio13@gmail.com>
2026-05-15 13:39:18 +0200

    Add Matriculacion component to Matriculacion page

---
+++ fronted/src/pages/Matriculacion.tsx
@@ -0,0 +1,12 @@
+import {Header} from "../components/Header/Header";
+import {Footer} from "../components/Footer/Footer";
+import Matriculacion from "../components/Registro/Matriculacion";
+
+const Matriculacion = () => (
+  <>
+    <Header/>
+    <Matriculacion />
+    <Footer/>
+  </>
+);
+export default Matriculacion;
